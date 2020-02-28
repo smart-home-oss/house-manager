1ad35f3256458cda0c72e696cf71949547d2bd90
andreicoval <andrei.coval@gmail.com>
2020-02-28 22:37:59 +0200

    started fixing tests

--- src/test/java/lu/smarthome/housemanager/houses/controllers/HouseControllerTest.java
+++ src/test/java/lu/smarthome/housemanager/houses/controllers/HouseControllerTest.java
@@ -5,10 +5,14 @@ import lu.smarthome.housemanager.houses.domain.House;
 import lu.smarthome.housemanager.houses.repository.HouseRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,6 +32,9 @@ class HouseControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void create() throws Exception {
