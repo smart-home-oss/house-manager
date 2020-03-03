037df4cda54eab50ae9998dffb32e1f0411f4dea
Rowebsoft <rowebsoft@gmail.com>
2020-03-03 20:58:03 +0100

    added active profiles

--- src/test/java/lu/smarthome/housemanager/HouseManagerApplicationTests.java
+++ src/test/java/lu/smarthome/housemanager/HouseManagerApplicationTests.java
@@ -2,12 +2,14 @@ package lu.smarthome.housemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"test1"})
 class HouseManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
