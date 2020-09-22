733d5273fbe6d643f7e0e7dce9f413274d110801
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-09-22 21:38:33 +0200

    align all projects to same line

--- src/test/java/lu/smarthome/housemanager/HouseManagerApplicationTests.java
+++ src/test/java/lu/smarthome/housemanager/HouseManagerApplicationTests.java
@@ -1,15 +1,55 @@
 package lu.smarthome.housemanager;
 
+import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles(profiles = {"test"})
 class HouseManagerApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
+    private Try<Customer> getCustomer() {
+        return getUser()
+                .mapTry(this::parseUser)
+                .map(user -> new Customer(user.getName()));
+    }
+
+    private Try<User> getUser() {
+        return Try.of(User::new);
+    }
+
+    private User parseUser(User user) throws Exception {
+        throw new Exception();
+    }
+
+}
+
+class Customer {
+    private String name;
+
+    public Customer(String name) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
